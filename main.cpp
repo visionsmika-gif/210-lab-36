3286924b35c8162d97858180980b7d1562fdb2c0
visionsmika-gif <visionsmika@gmail.com>
2025-11-25 16:55:57 -0800

    Milestone 3

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
     IntBinaryTree BST;      // Note: the class IntBinaryTree has been modified to hold string values rather than ints
     const string FILE_NAME = "codes.txt";
     
+    // Populate the BST with file
     ifstream codesFile;
     codesFile.open(FILE_NAME);
     if (!codesFile) {
@@ -20,41 +21,9 @@ int main() {
     }
     codesFile.close();
     
-    BST.displayPostOrder();
-
-
-    /*
-    // Test inserting nodes
-    BST.insertNode("Test String 1");
-    BST.insertNode("Test String 2");
-    BST.insertNode("Test String 3");
-
-    // Test displays
-    cout << "Inorder:\n";
+    // Output BST
     BST.displayInOrder();
-    cout << "Preorder:\n";
-    BST.displayPreOrder();
-    cout << "Postorder:\n";
-    BST.displayPostOrder();
 
-    // Test find nodes
-    if (BST.searchNode("Test String 3")) {
-        cout << "Test String 3 found\n";
-    }
-    else {
-        cout << "Test String 3 not found\n";
-    }
-
-    // Test removing nodes
-    cout << "Removing Test String 3:\n";
-    BST.remove("Test String 3");
-    if (BST.searchNode("Test String 3")) {
-        cout << "Test String 3 found\n";
-    }
-    else {
-        cout << "Test String 3 not found\n";
-    }
-    */
 
     return 0;
 }
