2f8d9c2ab624f6d5cb52cdcd395e2e83e812f3a2
visionsmika-gif <visionsmika@gmail.com>
2025-11-25 16:47:02 -0800

    update

--- main.cpp
+++ main.cpp
@@ -1,26 +1,29 @@
 #include <iostream>
+#include <fstream>
+#include <string>
 #include "IntBinaryTree.h"
 using namespace std;
 
-/* INSTRUCTIONS:
-This project contains the same dataset we used in Lab 25: codes comprised of letters and digits. It also contains the class definition file for the IntBinaryTree class.
-
-Modify the class to process the codes found in the codes.txt file, which are strings, rather than integers.
-
-Write a program that creates and populates a BST from these records.
-
-Add interactive, menu-driven functionality to add, delete, search, and modify records.
-
-Milestones to document with your commits, in addition to 10-minute commits (commit must read 'Milestone n' where n is the milestone number):
-
-Project is set up and running using the provided files.
-Code modification from int to strings is complete, and your code exercises this for testing.
-Your code successfully creates a BST from the records and outputs it using the method of your choosing.
-Your code's menu is functional.*/
-
 int main() {
     IntBinaryTree BST;      // Note: the class IntBinaryTree has been modified to hold string values rather than ints
+    const string FILE_NAME = "codes.txt";
+    
+    ifstream codesFile;
+    codesFile.open(FILE_NAME);
+    if (!codesFile) {
+        cout << "Unable to open the file " << FILE_NAME << ".\n";
+        return 1;
+    }
+    string code;
+    while (getline(codesFile, code)) {
+        BST.insertNode(code);
+    }
+    codesFile.close();
+    
+    BST.displayPostOrder();
 
+
+    /*
     // Test inserting nodes
     BST.insertNode("Test String 1");
     BST.insertNode("Test String 2");
@@ -51,6 +54,7 @@ int main() {
     else {
         cout << "Test String 3 not found\n";
     }
+    */
 
     return 0;
 }
